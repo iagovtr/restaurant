8985d7397f9946331806fad01f204d9c78a07ce6
iagovtr <iagovitor_araujo@hotmail.com>
2021-11-09 20:07:38 -0300

    Menu lateral - Desktop

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <CategoryMenu />
+  </div>
+</template>
+
+<script>
+  import CategoryMenu from './components/CategoryMenu.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      CategoryMenu
+    }
+  }
+</script>
+
+<style>
+  #app {}
+</style>
