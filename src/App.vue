879a9c1e93a4d1692a93f69821dd1dd2a479ea9f
iagovtr <iagovitor_araujo@hotmail.com>
2021-11-09 21:46:42 -0300

    Configuração de rotas/JSON server

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,9 @@
 <template>
   <div id="app">
-    <CategoryMenu />
+    <router-view />
   </div>
 </template>
 
-<script>
-  import CategoryMenu from './components/CategoryMenu.vue'
-
-  export default {
-    name: 'App',
-    components: {
-      CategoryMenu
-    }
-  }
-</script>
-
-<style>
+<style lang="less">
   #app {}
 </style>
