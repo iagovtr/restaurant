adf199d3ff6938a800dc46ce549b7ae69acc6b6e
iagovtr <iagovitor_araujo@hotmail.com>
2021-11-11 17:42:24 -0300

    Items List

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,26 @@
 <template>
   <div class="home">
     <CategoryMenu />
+    <ItemsList />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import CategoryMenu from "@/components/CategoryMenu.vue";
-import axios from "axios";
+import ItemsList from "@/components/ItemsList.vue";
 
 export default {
   name: "Home",
   components: {
     CategoryMenu,
-  },
-  created() {
-    axios.get("http://localhost:3000/burguers").then((response) => {
-      console.log(response);
-    });
+    ItemsList,
   },
 };
 </script>
+
+<style lang="less">
+.home {
+  display: flex;
+}
+</style>
